c99106bf4a39684444f23dd43e3976646113e0f7
Amitrath124 <amitrath344@gmail.com>
2025-08-04 21:48:39 +0530

    making navbar

---
+++ food_delivery/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'; 
+import {assets} from '../../assets/assets 2';
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+<img src={assets.logohome} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
