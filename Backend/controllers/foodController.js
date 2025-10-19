5d9efaddcc55f8df3ef1b65cf2008a2c9fbe24ea
Amitrath124 <amitrath344@gmail.com>
2025-10-19 14:02:47 +0530

    new admin folder created

--- Backend/controllers/foodController.js
+++ Backend/controllers/foodController.js
@@ -24,5 +24,27 @@ const addFood = async (req, res) => {
         res.status(500).json({ success: false, message: "Failed to add food item" });
     }
 }
+// all food list
+const listFood = async (req, res) => {
+ try {
+    const foods =  await foodModel.find({});
+    res.json({ success: true, data: foods });
+ } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Failed to fetch food items" });
+ }
+}
+//remove food item
+const removeFood = async (req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink(`uploads/${food.image}`, () => {});
+        await foodModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Food item removed successfully" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: "Error" });
+    }
 
-export { addFood };
+}
+export { addFood, listFood, removeFood };
